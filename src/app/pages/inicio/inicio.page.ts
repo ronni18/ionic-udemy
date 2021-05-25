9b3520afd19bbc500d35b93852bd7b207237e9cb
roni18 <ronnicarrascaltejedor@gmail.com>
2021-05-25 16:29:40 -0500

    se sustituyo los botones por una lista

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -1,11 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
+interface components {
+  icon: string;
+  name: string;
+  redirectTo: string;
+
+  
+}
+
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.page.html',
   styleUrls: ['./inicio.page.scss'],
 })
 export class InicioPage implements OnInit {
+
+
+  componentes: components[]= [
+    {
+      icon: 'american-football-outline',
+      name: 'Action Sheet',
+      redirectTo: '/action-sheet',
+    },
+    {
+      icon: 'logo-apple-appstore',
+      name: 'Alert',
+      redirectTo: '/alert',
+    }
+  ];
 
   constructor() { }
 
